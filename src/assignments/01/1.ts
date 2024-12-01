d0e43977a10a9890e7e481458d870d5a58aaef27
Rasmus Svanberg <rasmus.svahnberg97@gmail.com>
2024-12-01 12:15:38 +0100

    Initial commit

---
+++ src/assignments/01/1.ts
@@ -0,0 +1,4 @@
+export default async function (): Promise<void> {
+    console.log("Hello world!");
+    const hej = "sdas";
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Day01_1 from "./assignments/01/1";
+
+Day01_1();
