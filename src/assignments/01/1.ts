44eb5a5408e3397e9f47dba73b4181cfcdc3254b
Rasmus Svanberg <rasmus.svahnberg97@gmail.com>
2024-12-01 21:31:52 +0100

    Day 1

--- src/assignments/01/1.ts
+++ src/assignments/01/1.ts
@@ -1,8 +1,27 @@
 import fs from "fs";
 
-// const FILE_PATH = './src/resources/01_example.txt'
+// const FILE_PATH = "./src/resources/01_example.txt";
 const FILE_PATH = "./src/resources/01.txt";
 
 export default async function (): Promise<void> {
     const lines = fs.readFileSync(FILE_PATH, "utf-8").trim().split("\n");
+
+    let listLeft: number[] = [];
+    let listRight: number[] = [];
+
+    lines.forEach((line) => {
+        const [left, right] = line.split(/\s+/);
+        listLeft.push(parseInt(left));
+        listRight.push(parseInt(right));
+    });
+
+    listLeft = listLeft.sort((a, b) => a - b);
+    listRight = listRight.sort((a, b) => a - b);
+
+    let total = 0;
+    for (let i = 0; i < listLeft.length; i++) {
+        total += Math.abs(listRight[i] - listLeft[i]);
+    }
+
+    console.log("Total is: ", total);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import Day01_1 from "./assignments/01/1";
+import Day01_2 from "./assignments/01/2";
 
-Day01_1();
+import Day02_1 from "./assignments/01/1";
+import Day02_2 from "./assignments/01/2";
+
+/** Day 1 */
+// Day01_1();
+// Day01_2();
+
+/** Day 1 */
+// Day02_1();
+// Day02_2();
